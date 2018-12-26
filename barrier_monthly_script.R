c97e4d9033961a5c092545ad113725829aa861ba
Sam Bankston <sebankston@gmail.com>
2018-12-26 15:59:22 -0800

    updates

--- barrier_monthly_script.R
+++ barrier_monthly_script.R
@@ -15,7 +15,16 @@ sheet_titles <- streams_list$sheet_title
 
 all_data <- map(sheet_titles, ~ gs_title(.x) %>% gs_read(ws = "Sheet1") %>% clean_names()) %>% set_names(nm = sheet_titles)
 
+<<<<<<< HEAD
 bar_sum <- all_data %>% map_df(., ~.x %>% group_by(barrier_type, date_assessed) %>% summarize(count = n()), .id = "id") %>% ungroup()
+=======
+barrier_summary <- all_data %>% map_df(., ~.x %>% group_by(barrier_type) %>% summarize(count = n()) 
+, .id = "id") 
+
+barrier_summary <- barrier_summary %>% rename(stream = id)
+
+View(barrier_summary)
+>>>>>>> 93b1891d58fc6d0bd2876e6d2aa9f23f05936742
 
 bar_sum_current <- bar_sum %>% rename(stream = id) %>% mutate(date_assessed = mdy(date_assessed)) %>% filter(month(date_assessed) == month(Sys.Date())) %>% group_by(stream, barrier_type) %>% summarize(total = sum(count))
 
--- barrier_monthly_script_2.R
+++ barrier_monthly_script_2.R
@@ -9,10 +9,10 @@ library(lubridate)
 
 all_sheets <- as_tibble(gs_ls())
 
-streams_list <- all_sheets %>% filter(str_detect(.$sheet_title, pattern = "inventory")) %>% group_by(sheet_title)
+streams_list <- all_sheets %>% filter(str_detect(.$sheet_title, pattern = paste0(c("inventory", "Inventory"), collapse = "|"))) %>% group_by(sheet_title)
 
-sheet_titles <- streams_list$sheet_title
-
+sheet_titles <- streams_list %>% filter(str_detect(sheet_title, pattern = "ry$")) %>% pull(sheet_title)
+                                                    
 all_data <- map(sheet_titles, ~ gs_title(.x) %>% gs_read(ws = "Sheet1") %>% clean_names()) %>% set_names(nm = sheet_titles)
 
 bar_sum <- all_data %>% map_df(., ~.x %>% group_by(barrier_type, date_assessed) %>% summarize(count = n()), .id = "id") %>% ungroup()
@@ -20,3 +20,5 @@ bar_sum <- all_data %>% map_df(., ~.x %>% group_by(barrier_type, date_assessed) 
 bar_sum_current <- bar_sum %>% rename(stream = id) %>% mutate(date_assessed = mdy(date_assessed)) %>% filter(month(date_assessed) == month(Sys.Date())) %>% group_by(stream, barrier_type) %>% summarize(total = sum(count))
 
 View(bar_sum_current)
+
+# all_data %>% map(~colnames(.x))
